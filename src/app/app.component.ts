4dc2af4c01130ccdfcebb71f92782f2838366e89
Junior98-dev <kounkouddorianjunior@gmail.com>
2025-01-18 00:43:12 +0100

    Configuration de fontAwesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './layout/navigation/navigation.component';
-
+import { fontAwesomeIcons } from './shared/font-awesome-icones';
+import { FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome"
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavigationComponent],
+  imports: [RouterOutlet,
+    NavigationComponent,
+    FontAwesomeModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'app-vacance-Booking';
+  
+  private faIconLibrary:FaIconLibrary = inject(FaIconLibrary);
+
+  ngOnInit(): void{
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() : void {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
+
 }
